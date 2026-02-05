16ffe45003d0d12829063d0af83cc5102ee2a387
2025ab05078-gif <2025ab05078@wilp.bits-pilani.ac.in>
2026-02-05 13:17:59 +0530

    Remove temporary code for column display
    Removed temporary code for displaying columns in the uploaded file.

--- app.py
+++ app.py
@@ -46,10 +46,8 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, sep=";")
-   #temp code added
-    st.write("Columns found:", df.columns.tolist())
-    #temp code end
-
+    df.columns = df.columns.str.strip()
+ 
     st.subheader("📄 Uploaded Dataset Preview")
     st.dataframe(df.head())
 
